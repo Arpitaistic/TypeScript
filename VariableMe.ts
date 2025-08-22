7b32db8c7687e4f3e8332c89356f4467451eca3e
Arpita <arpitaistic@gmail.com>
2025-08-22 08:48:49 +0530

    Any in TypeScript

--- VariableMe.ts
+++ VariableMe.ts
@@ -6,3 +6,10 @@ let userID: Number = 43738;
 
 //Boolean
 let isLoggedIn: Boolean = true;
+
+//Any: Can hold any data type!
+let hero: string;
+function getHero(){
+    hero = "Spiderman";
+    return hero;
+}
