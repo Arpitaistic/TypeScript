a02304da0329926fc19b791d240ccf96d7f27932
Arpita <arpitaistic@gmail.com>
2025-08-22 09:31:09 +0530

    Objects in TypeScript

---
+++ 02_Basics/myObjects.ts
@@ -0,0 +1,14 @@
+const userDetails = {
+    name: "Arpita",
+    email: "arpitaistic@gmail.com",
+    isActive: true,
+}
+
+function createUser ({name: String, isPaid: boolean}){}
+createUser({name: "Arpita", isPaid: false});
+
+function createCourse(): {name; string, price: number}{
+    return {name: "ReactJS", price: 399};
+}
+
+export {}
