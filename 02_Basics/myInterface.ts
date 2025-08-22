5ac1f7a1714591d19fb728068430e9fb1c6ef149
Arpita <arpitaistic@gmail.com>
2025-08-22 12:02:08 +0530

    More on Interface

--- 02_Basics/myInterface.ts
+++ 02_Basics/myInterface.ts
@@ -6,9 +6,16 @@ interface helloUser {
    // startTrail: ()=> string
    startTrail(): string
    getCoupon(couponname: string, value: number): number
+  
 }
-
-const arpitaa: helloUser = { dbID: 383743, email: "arpitaistic@gmail.com", userID: 22 ;
+interface helloUser{ //Reopening of the interface
+     githubToken: string
+}
+interface AdminDetails extends helloUser {
+    role: "admin" | "ta"| "learner"
+}
+const arpitaa: helloUser = { dbID: 383743, email: "arpitaistic@gmail.com", userID: 22, 
+    githubToken: "Github",
 startTrail: ()=>{
     return "Trail Started!"
 }, getCoupon:(name: "Arpita10", off: 10)=>{
