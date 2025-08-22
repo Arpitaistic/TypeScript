892960ba2bf548cfa1e31d1af2c3f5d913e43dca
Arpita <arpitaistic@gmail.com>
2025-08-22 10:54:45 +0530

    Tuples in TypeScript

---
+++ 02_Basics/myTuples.ts
@@ -0,0 +1,9 @@
+let tupleUser: [string, number, boolean]
+tupleUser = ["Arpita", 334, true]
+
+let rgb: [number, number, number] = [22433, 3535, 75948]
+type tupleUser = [number, string]
+
+const newUser: tupleUser = [112, "example@gmail.com"]
+newUser [1] = "ar@gmail.com"
+newUser.push(true);
